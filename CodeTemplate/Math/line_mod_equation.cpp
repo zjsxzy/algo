13c17e955a4a842f99f6232ec1f87f6b0ccf865d
zjsxzy <zjsxzy@gmail.com>
2013-09-19 23:03:32 +0800

    add mod equation

---
+++ CodeTemplate/Math/line_mod_equation.cpp
@@ -0,0 +1,9 @@
+long long line_mod_equation(long long a, long long b, long long n) {
+	long long x, y;
+	long long d = extend_gcd(a, n, x, y);
+	if (b % d == 0) {
+		LL r = n / d;
+		return (x * (b / d) % r + r) % r;
+	}
+	return -1;
+}
