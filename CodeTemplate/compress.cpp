4636b733e18e0f541485b12a456de5d1243fc3ea
zjsxzy <zjsxzy@gmail.com>
2013-12-29 14:34:02 +0800

    change compress

--- CodeTemplate/compress.cpp
+++ CodeTemplate/compress.cpp
@@ -1,10 +1,10 @@
-void compress(int as[], int n, int &L) {
+void compress(int as[], int n) {
 	vector<int> num;
 	for (int i = 0; i < n; i++) {
 		num.PB(as[i]);
 	}
 	sort(num.begin(), num.end());
-	L = unique(num.begin(), num.end()) - num.begin();
+    num.resize(unique(num.begin(), num.end()) - num.begin());
 	for (int i = 0; i < n; i++) {
 		as[i] = lower_bound(num.begin(), num.end(), as[i]) - num.begin() + 1;
 	}
