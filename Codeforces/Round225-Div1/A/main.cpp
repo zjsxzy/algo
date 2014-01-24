74206d164ddc6f28a035fd15e508e33e4e55cff0
zjsxzy <zjsxzy@gmail.com>
2014-01-24 21:12:53 +0800

    Round225 div1 A

---
+++ Codeforces/Round225-Div1/A/main.cpp
@@ -0,0 +1,70 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+const int maxn = 200000 + 5;
+struct BinaryIndexTree {
+	int N;
+	int C[maxn];
+	
+	void init(int N) {
+		this->N = N;
+		memset(C, 0, sizeof(C));
+	}
+	
+	int lowbit(int x) {
+		return x & (-x);
+	}
+	
+	void add(int x, int num) {
+		for (int i = x; i <= N; i += lowbit(i))
+			C[i] += num;
+	}
+	
+	int sum(int x) {
+		int ret = 0;
+		for (int i = x; i > 0; i -= lowbit(i))
+			ret += C[i];
+		return ret;
+	}
+}bit;
+int arr[maxn];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+	LL ret = 0;
+	bit.init(n);
+	for (int i = 1; i <= n; i++) {
+		if (arr[i] == 0) {
+			ret += (LL)bit.sum(i);
+		} else {
+			bit.add(i, 1);
+		}
+	}
+	cout << ret << endl;
+	return 0;
+}
+
