2cfa75cd3f903d966a5411b0ac0ede3d27519604
zjsxzy <zjsxzy@gmail.com>
2014-01-25 17:16:30 +0800

    SRM605 div2 500pt

---
+++ Topcoder/AlienAndGame.cpp
@@ -0,0 +1,87 @@
+#include <map>
+#include <set>
+#include <list>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+class AlienAndGame {
+public:
+    int getNumber (vector <string> board) {
+		int n = board.size(), m = board[0].size();
+		for (int l = min(n, m); l >= 1; l--) {
+			for (int i = 0; i + l - 1 < n; i++) {
+				for (int j = 0; j + l - 1 < m; j++) {
+					bool ok = true;
+					for (int x = i; x < i + l; x++) {
+						for (int y = j + 1; y < j + l; y++) {
+							if (board[x][y] != board[x][y - 1]) {
+								ok = false;
+								break;
+							}
+							if (!ok) break;
+						}
+					}
+					if (ok) return l * l;
+				}
+			}
+		}
+		return 1;
+    }
+
+
+    
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"BB",
+ "WW"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; verify_case(0, Arg1, getNumber(Arg0)); }
+	void test_case_1() { string Arr0[] = {"W"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(1, Arg1, getNumber(Arg0)); }
+	void test_case_2() { string Arr0[] = {"WBBB",
+ "WBBB",
+ "WWWW"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 9; verify_case(2, Arg1, getNumber(Arg0)); }
+	void test_case_3() { string Arr0[] = {"W",
+ "B",
+ "W",
+ "W",
+ "W"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(3, Arg1, getNumber(Arg0)); }
+	void test_case_4() { string Arr0[] = {"BWBBWBB",
+ "WWBWWBW",
+ "BBBBBBW",
+ "WBBBBWB",
+ "BBWWWWB",
+ "WWWWWWW",
+ "BBWWBBB"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 9; verify_case(4, Arg1, getNumber(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main() {
+    AlienAndGame ___test;
+    for (int i = 0; i < 6; i++)
+        ___test.run_test(i);
+    return 0;
+}
+// END CUT HERE
