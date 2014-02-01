2bc17b0971e7e4173d900b18dcdcdcd0c44ddde2
zjsxzy <zjsxzy@gmail.com>
2014-02-01 19:35:26 +0800

    Round227 div2 B

---
+++ Codeforces/Round227-Div2/B/main.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n), b(m);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = 0; i < m; i++)
+		cin >> b[i];
+	int ia = 0, ib = 0;
+	for (; ia < n && ib < m; ib++) {
+		if (a[ia] <= b[ib]) ia++;
+	}
+	cout << n - ia << endl;
+	return 0;
+}
+
