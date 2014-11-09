b94f6491b1f9c525cf4934cd348f53db9082e2b2
zhangyang <zjsxzy@gmail.com>
2014-11-09 14:10:35 +0800

    change segtree

--- CodeTemplate/DataStucture/SegTree.cpp
+++ CodeTemplate/DataStucture/SegTree.cpp
@@ -1,34 +1,52 @@
-const int MAXN = 50005;
+const int MAXN = 5005;
 struct SegTree {
 
-	int sum[MAXN << 2], flag[MAXN << 2];
+	int sum[MAXN << 2], add[MAXN << 2];
 
-	int lson(int rt) {return rt << 1; }
-	int rson(int rt) {return rt << 1 | 1; }
+	int lson(int rt) {
+		return rt << 1;
+	}
+
+	int rson(int rt) {
+		return rt << 1 | 1;
+	}
 
 	void pushUp(int rt) {
 		sum[rt] = sum[lson(rt)] + sum[rson(rt)];
 	}
 
 	void pushDown(int rt, int m) {
-		if (flag[rt] != -1) {
-			flag[lson(rt)] = flag[rt];
-			flag[rson(rt)] = flag[rt];
-			sum[lson(rt)] = (m - (m >> 1)) * flag[rt];
-			sum[rson(rt)] = (m >> 1) * flag[rt];
-			flag[rt] = -1;
+		if (add[rt]) {
+			add[lson(rt)] += add[rt];
+			add[rson(rt)] += add[rt];
+			sum[lson(rt)] += add[rt] * (m - (m >> 1));
+			sum[rson(rt)] += add[rt] * (m >> 1);
+			add[rt] = 0;
 		}
 	}
 
 	void build(int l, int r, int rt) {
-		flag[rt] = -1;
+		add[rt] = 0;
+		sum[rt] = 0;
 		if (l == r) {
-			sum[rt] = 0;
 			return;
 		}
 		int mid = (l + r) >> 1;
 		build(l, mid, lson(rt));
 		build(mid + 1, r, rson(rt));
+		pushUp(rt);
+	}
+
+	void update(int L, int R, int c, int l, int r, int rt) {
+		if (L <= l && r <= R) {
+			add[rt] += c;
+			sum[rt] += c * (r - l + 1);
+			return;
+		}
+		pushDown(rt, r - l + 1);
+		int mid = (l + r) >> 1;
+		if (L <= mid) update(L, R, c, l, mid, lson(rt));
+		if (R > mid) update(L, R, c, mid + 1, r, rson(rt));
 		pushUp(rt);
 	}
 
@@ -38,22 +56,9 @@ struct SegTree {
 		}
 		pushDown(rt, r - l + 1);
 		int mid = (l + r) >> 1;
-		int ret = 0;
-		if (L <= mid) ret += query(L, R, l, mid, lson(rt));
-		if (R > mid) ret += query(L, R, mid + 1, r, rson(rt));
-		return ret;
-	}
-
-	void update(int L, int R, int c, int l, int r, int rt) {
-		if (L <= l && r <= R) {
-			flag[rt] = c;
-			sum[rt] = c * (r - l + 1);
-			return;
-		}
-		pushDown(rt, r - l + 1);
-		int mid = (l + r) >> 1;
-		if (L <= mid) update(L, R, c, l, mid, lson(rt));
-		if (R > mid) update(L, R, c, mid + 1, r, rson(rt));
-		pushUp(rt);
+		int res = 0;
+		if (L <= mid) res += query(L, R, l, mid, lson(rt));
+		if (mid < R) res += query(L, R, mid + 1, r, rson(rt));
+		return res;
 	}
 }tree;
