2a4ad1ea2cff85ec48ff4da74f5dc5742ae001a7
zhangyang <zjsxzy@gmail.com>
2015-03-01 09:29:51 +0800

    覆盖集独立集

---
+++ hihoCoder/Prob35/main.cpp
@@ -0,0 +1,73 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+const int MAXN = 1000 + 5;
+int n, m;
+int mk[MAXN], mate[MAXN];
+vector<int> adj[MAXN];
+void addEdge(int u, int v) {
+    adj[u].PB(v);
+}
+int path(int u) {
+	for (int i = 0, v; i < adj[u].size(); i++) {
+		if (!mk[v = adj[u][i]]) {
+			mk[v] = 1;
+			if (mate[v] == -1 || path(mate[v])) {
+				mate[v] = u;
+				mate[u] = v;
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+int MaxMatch() {
+	memset(mate, -1, sizeof(mate));
+	int ret = 0;
+	for (int i = 0; i < n; i++) {
+		if (mate[i] == -1) {
+			memset(mk, 0, sizeof(mk));
+			ret += path(i);
+		}
+	}
+	return ret;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+#endif
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+		a--; b--;
+        addEdge(a, b);
+        addEdge(b, a);
+    }
+    int ret = MaxMatch();
+    printf("%d\n%d\n", ret, n - ret);
+	return 0;
+}
+
