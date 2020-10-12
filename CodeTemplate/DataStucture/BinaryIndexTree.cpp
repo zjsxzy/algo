7394424ee10cc080d4ba79bd31e6b564f8958214
zhangyang <zjsxzy@gmail.com>
2020-10-12 17:39:04 +0800

    change bit

--- CodeTemplate/DataStucture/BinaryIndexTree.cpp
+++ CodeTemplate/DataStucture/BinaryIndexTree.cpp
@@ -5,21 +5,21 @@ const int maxn = 1000005;
 struct BinaryIndexTree {
 	int N;
 	int C[maxn];
-	
+
 	void init(int N) {
 		this->N = N;
 		memset(C, 0, sizeof(C));
 	}
-	
+
 	int lowbit(int x) {
 		return x & (-x);
 	}
-	
+
 	void add(int x, int num) {
 		for (int i = x; i <= N; i += lowbit(i))
 			C[i] += num;
 	}
-	
+
 	int sum(int x) {
 		int ret = 0;
 		for (int i = x; i > 0; i -= lowbit(i))
