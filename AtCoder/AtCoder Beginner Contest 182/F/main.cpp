b9001ee05bc555854ca23bb80e6a41a883d875ce
zjsxzy <zjsxzy@gmail.com>
2020-11-09 00:26:03 +0800

    add f

---
+++ AtCoder/AtCoder Beginner Contest 182/F/main.cpp
@@ -0,0 +1,42 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n;
+    LL X;
+    cin >> n >> X;
+    vector<LL> a(n+1), d(n+1);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        d[i] = X / a[i];
+        X %= a[i];
+    }
+    vector<vector<LL> > dp(n, vector<LL>(2));
+    cout << dp[n][0] << endl;
+    return 0;
+}
+
