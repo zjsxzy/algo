d7b8f1bd42fe39747d8f0bb891794ae71eaad810
zjsxzy <zjsxzy@gmail.com>
2020-11-09 22:06:02 +0800

    add D

--- AtCoder/AtCoder Beginner Contest 155/D/main.cpp
+++ AtCoder/AtCoder Beginner Contest 155/D/main.cpp
@@ -34,30 +34,37 @@ LL count(LL val) {
     if (val == 0) {
         sum = neg_sz * pos_sz;
     } else if (val > 0) {
-        sum = zeros * (neg_sz + pos_sz) + neg_sz * pos_sz;
+        sum = zeros * (neg_sz + pos_sz) + neg_sz * pos_sz + zeros * (zeros - 1) / 2;
         // cout << sum << endl;
         for (int i = 0; i < pos_sz; i++) {
             LL px = pos[i];
             LL t = val / px;
             LL cnt = upper_bound(pos.begin(), pos.end(), t) - pos.begin();
-            // cout << t << " " << cnt - i - 1 << endl;
-            sum += max(0LL, cnt - i - 1);
+            // cout << px << " " << t << " " << cnt << endl;
+            if (cnt > i) {
+                sum += cnt - i - 1;
+            }
         }
         // cout << sum << endl;
         for (int i = 0; i < neg_sz; i++) {
             LL nx = neg[i];
             LL t = val / nx;
             LL cnt = lower_bound(neg.begin(), neg.end(), t) - neg.begin();
-            sum += min(neg_sz - i - 1, neg_sz - cnt);
+            // cout << t << " " << cnt << endl;
+            if (cnt > i) {
+                sum += neg_sz - cnt;
+            } else {
+                sum += neg_sz - i - 1;
+            }
         }
+        // cout << sum << endl;
     } else if (val < 0) {
         for (int i = 0; i < neg_sz; i++) {
             LL nx = neg[i];
             LL t = val / nx;
-            LL cnt = lower_bound(pos.begin(), pos.end(), t) - pos.begin();
-            cout << t << " " << cnt << endl;
-            sum += min(pos_sz - i - 1, pos_sz - cnt);
-            cout << sum << endl;
+            LL cnt = upper_bound(pos.begin(), pos.end(), t) - pos.begin();
+            // cout << t << " " << cnt << endl;
+            sum += pos_sz - cnt;
         }
     }
     return sum;
@@ -66,36 +73,47 @@ LL count(LL val) {
 int main() {
     cin >> n >> k;
     zeros = 0;
+    map<LL, int> cnt;
     for (int i = 0; i < n; i++) {
         LL x;
         cin >> x;
+        cnt[x]++;
         if (x > 0) pos.push_back(x);
         else if (x < 0) neg.push_back(x);
         else zeros++;
     }
     sort(neg.begin(), neg.end());
-    sort(pos.begin(), pos.begin());
+    sort(pos.begin(), pos.end());
     pos_sz = (LL)pos.size(), neg_sz = (LL)neg.size();
 
-    cout << count(5) << endl;
-    cout << count(8) << endl;
-    cout << count(9) << endl;
-    cout << count(-1) << endl;
-    cout << count(-10) << endl;
-    cout << count(-13) << endl;
-    /*
     LL lo = -INF, hi = INF;
     LL res;
     while (lo <= hi) {
         LL mid = (lo + hi) >> 1;
-        if (count(mid) <= k) {
+        LL t = count(mid);
+        if (t >= k) {
             res = mid;
-            lo = mid + 1;
-        } else {
             hi = mid - 1;
+        } else {
+            lo = mid + 1;
         }
     }
+    bool find = false;
+    for (auto x : cnt) {
+        if (x.first != 0 && res % x.first == 0) {
+            int r = res / x.first;
+            if (r == x.first) {
+                if (cnt[r] > 1) find = true;
+            } else {
+                if (cnt.find(r) != cnt.end()) {
+                    find = true;
+                }
+            }
+        }
+    }
+    if (!find) res--;
+    //cout << find << endl;
+    //cout << res << " " << lo << " " << hi << endl;
     cout << res << endl;
-    */
     return 0;
 }
