344c506be7cdd699b6e0d3cabb935760f26971c2
Zhang Yang <zjsxzy@gmail.com>
2020-11-16 13:23:34 +0800

    update e

--- AtCoder/AtCoder Beginner Contest 183/E/main.cpp
+++ AtCoder/AtCoder Beginner Contest 183/E/main.cpp
@@ -27,7 +27,7 @@ const int maxn = 2005;
 const LL mod = 1000000007;
 int h, w;
 string s[maxn];
-LL dp[maxn][maxn];
+LL dp[maxn][maxn], dprs[maxn][maxn], dpcs[maxn][maxn], dpds[maxn][maxn];
 
 int main() {
     cin >> h >> w;
@@ -38,11 +38,34 @@ int main() {
     dp[0][0] = 1;
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
-            if (s[i][j] == '.' && dp[i][j] > 0) {
-
+            if (s[i][j] == '#') continue;
+            if (i - 1 >= 0) {
+                dp[i][j] = (dp[i][j] + dprs[i - 1][j]) % mod;
+            }
+            if (j - 1 >= 0) {
+                dp[i][j] = (dp[i][j] + dpcs[i][j - 1]) % mod;
+            }
+            if (i - 1 >= 0 && j - 1 >= 0) {
+                dp[i][j] = (dp[i][j] + dpds[i - 1][j - 1]) % mod;
+            }
+            if (i - 1 >= 0) {
+                dprs[i][j] = (dprs[i - 1][j] + dp[i][j]) % mod;
+            } else {
+                dprs[i][j] = dp[i][j];
+            }
+            if (j - 1 >= 0) {
+                dpcs[i][j] = (dpcs[i][j - 1] + dp[i][j]) % mod;
+            } else {
+                dpcs[i][j] = dp[i][j];
+            }
+            if (i - 1 >= 0 && j - 1 >= 0) {
+                dpds[i][j] = (dpds[i - 1][j - 1] + dp[i][j]) % mod;
+            } else {
+                dpds[i][j] = dp[i][j];
             }
         }
     }
+    cout << dp[h - 1][w - 1] << endl;
     return 0;
 }
 
