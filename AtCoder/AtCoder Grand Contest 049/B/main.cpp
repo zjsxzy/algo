704f2ef86fd8d2b544bb0e61ea9beb8cdeb69518
zhangyang <zjsxzy@gmail.com>
2020-11-19 16:59:09 +0800

    add B

---
+++ AtCoder/AtCoder Grand Contest 049/B/main.cpp
@@ -0,0 +1,64 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n;
+    string s, t;
+    cin >> n;
+    cin >> s >> t;
+    queue<int> q;
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '1') {
+            q.push(i);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (t[i] == '1') {
+            if (q.empty()) {
+                cout << -1 << endl;
+                return 0;
+            }
+            int x = q.front(); q.pop();
+            while (x < i) {
+                if (q.empty()) {
+                    cout << -1 << endl;
+                    return 0;
+                }
+                int y = q.front(); q.pop();
+                res += y - x;
+                if (!q.empty()) {
+                    x = q.front(); q.pop();
+                }
+            }
+            if (x < i) {
+                cout << -1 << endl;
+                return 0;
+            }
+            res += x - i;
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
+
