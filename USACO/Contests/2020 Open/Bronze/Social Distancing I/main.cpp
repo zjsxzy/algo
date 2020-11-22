772f38ac842a6088fc4e61909506e3dcb49ad57a
zjsxzy <zjsxzy@gmail.com>
2020-11-22 15:07:21 +0800

    update

--- USACO/Contests/2020 Open/Bronze/Social Distancing I/main.cpp
+++ USACO/Contests/2020 Open/Bronze/Social Distancing I/main.cpp
@@ -36,7 +36,10 @@ int main() {
     for (int i = 0; i <= n; i++) {
         if (s[i] == '1') {
             int dis = i - prev;
-            if (prev == -1 || i == n) {
+            if (prev == -1 && i == n) { // no 1
+                cout << n - 1 << endl;
+                return 0;
+            } if (prev == -1 || i == n) {
                 one.push_back(dis - 1);
                 two.push_back((dis - 1) / 2);
             } else {
@@ -49,10 +52,6 @@ int main() {
     }
     sort(one.begin(), one.end());
     sort(two.begin(), two.end());
-    if (prev == n) { // no 1
-        cout << n - 1 << endl;
-        return 0;
-    }
     int res2 = 0;
     if (two.size() > 0) {
         res2 = max(res2, two[two.size() - 1]);
