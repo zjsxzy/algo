83f4197e6c72ae16448c3e100991dd65add58c93
zjsxzy <zjsxzy@gmail.com>
2020-11-24 00:02:56 +0800

    add dijkstra

---
+++ CodeTemplate/GraphTheory/Dijkstra.cpp
@@ -0,0 +1,25 @@
+struct edge {
+    int to, cost;
+};
+int dijkstra(int st, int ed) {
+    vector<vector<edge> > adj(n);
+    // construct graph: adj[u].push_back({v, c})
+    vector<int> dist(adj.size(), inf);
+    dist[st] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, st});
+    while (!pq.empty()) {
+        auto p = pq.top(); pq.pop();
+        int v = p.second;
+        if (p.first > dist[v]) continue;
+        for (edge e : adj[v]) {
+            int nd = p.first + e.cost;
+            if (nd < dist[e.to]) {
+                dist[e.to] = nd;
+                pq.push({nd, e.to});
+            }
+        }
+    }
+    return dist[ed];
+}
+
