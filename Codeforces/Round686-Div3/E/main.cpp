cd31811ef50ee2cb901649f3f2384ffede2cfd31
Zhang Yang <zjsxzy@gmail.com>
2020-11-25 18:33:22 +0800

    add E

--- Codeforces/Round686-Div3/E/main.cpp
+++ Codeforces/Round686-Div3/E/main.cpp
@@ -21,17 +21,44 @@ using namespace std;
 #define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
 typedef long long LL;
 
-const int MAXN = 200005;
-int n;
-vector<int> adj[MAXN];
-
 void solve() {
+	int n;
     cin >> n;
+	vector<vector<int> > adj(n);
+	vector<int> deg(n, 0);
     for (int i = 0; i < n; i++) {
         int u, v;
         cin >> u >> v;
+		u--; v--;
         adj[u].push_back(v);
+		adj[v].push_back(u);
+		deg[u]++; deg[v]++;
     }
+	vector<LL> cnt(n, 1);
+	queue<int> q;
+	for (int i = 0; i < n; i++) {
+		if (deg[i] == 1) {
+			q.push(i);
+		}
+	}
+	while (!q.empty()) {
+		int u = q.front(); q.pop();
+		for (auto v : adj[u]) {
+			deg[v]--;
+			if (deg[v] == 1) {
+				q.push(v);
+			}
+			cnt[v] += cnt[u];
+		}
+	}
+	LL res = 0;
+	for (int i = 0; i < n; i++) {
+		if (deg[i] == 2) { // vertex on circle
+			res += cnt[i] * (cnt[i] - 1) / 2;
+			res += cnt[i] * (n - cnt[i]);
+		}
+	}
+	cout << res << endl;
 }
 
 int main() {
