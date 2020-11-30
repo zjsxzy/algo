a8384e36759b3d4b421ef546669bf94610c705ee
Zhang Yang <zjsxzy@gmail.com>
2020-11-30 18:58:35 +0800

    add c

--- AtCoder/AtCoder Regular Contest 109/C/main.cpp
+++ AtCoder/AtCoder Regular Contest 109/C/main.cpp
@@ -21,12 +21,51 @@ using namespace std;
 #define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
 typedef long long LL;
 
-void solve() {
-    int n, k;
-    cin >> n >> k;
-    string s;
-    cin >> s;
+const int MAXN = 105;
+char dp[MAXN][MAXN];
+int pw[MAXN];
+int n, k;
+string s;
+// map<pair<int, int>, char> f;
 
+char win(char a, char b) {
+	if (a == b) return a;
+	if (a == 'R' && b == 'P') return 'P';
+	if (a == 'P' && b == 'S') return 'S';
+	if (a == 'S' && b == 'R') return 'R';
+	return a;
+}
+
+char dfs(int i, int j) {
+	if (dp[i][j] != '!') return dp[i][j];
+	//if (f.find({i, j}) != f.end()) {
+	//	return f[{i, j}];
+	//}
+	if (j == 0) {
+		return s[i];
+	}
+	char l = dfs(i, j - 1);
+	char r = dfs((i + pw[j - 1]) % n, j - 1);
+	char res = win(l, r);
+	//f[{i, j}] = res;
+	dp[i][j] = res;
+	return res;
+}
+
+void solve() {
+    cin >> n >> k;
+    cin >> s;
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= k; j++) {
+			dp[i][j] = '!';
+		}
+	}
+	pw[0] = 1;
+	for (int i = 1; i <= k; i++) {
+		pw[i] = pw[i - 1] * 2 % n;
+	}
+	char res = dfs(0, k);
+	cout << res << endl;
 }
 
 int main() {
