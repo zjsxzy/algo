0e3fbe8d2a58a1e6b81eba1abec3370fa52ce4ef
zjsxzy <zjsxzy@gmail.com>
2020-12-01 23:40:18 +0800

    add educational round 99

---
+++ Codeforces/Educational Round 99/A/main.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+void solve() {
+    string n;
+    cin >> n;
+    cout << n.size() << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    int ts;
+    cin >> ts;
+    while (ts--) {
+        solve();
+    }
+    return 0;
+}
+
---
+++ Codeforces/Educational Round 99/B/main.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+void solve() {
+    int x;
+    cin >> x;
+    int sum = 0;
+    int i;
+    for (i = 1; sum < x; i++) {
+        sum += i;
+    }
+    if (sum == x + 1) cout << i << endl;
+    else cout << i - 1 << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    int ts;
+    cin >> ts;
+    while (ts--) {
+        solve();
+    }
+    return 0;
+}
+
