53bb30a1dd9ca708e5de0b048f6822e64ce8e868
zjsxzy <zjsxzy@gmail.com>
2020-12-05 23:19:20 +0800

    update round 688

--- Codeforces/Round688-Div2/C/main.cpp
+++ Codeforces/Round688-Div2/C/main.cpp
@@ -24,56 +24,62 @@ void solve() {
     int n;
     cin >> n;
     vector<string> mat(n);
-    vector<int> res(10);
     for (int i = 0; i < n; i++) {
         cin >> mat[i];
     }
-    vector<int> min_row(10, -1);
+    vector<int> min_row(10, -1), max_row(10, -1), min_col(10, -1), max_col(10, -1);
     for (int i = 0; i < n; i++) {
-        vector<int> min_col(10, -1), max_col(10, -1);
         for (int j = 0; j < n; j++) {
-            int c = mat[i][j] - '0';
-            if (min_row[c] == -1) min_row[c] = i;
-            if (min_col[c] == -1) min_col[c] = j;
-            max_col[c] = j;
+            int t = mat[i][j] - '0';
+            if (min_row[t] == -1) min_row[t] = i;
+            else min_row[t] = min(min_row[t], i);
+            if (min_col[t] == -1) min_col[t] = j;
+            else min_col[t] = min(min_col[t], j);
+            max_row[t] = max(max_row[t], i);
+            max_col[t] = max(max_col[t], j);
+        }
+    }
+
+    vector<int> res(10);
+    // by row
+    for (int i = 0; i < n; i++) {
+        map<int, vector<int>> vec;
+        for (int j = 0; j < n; j++) {
+            vec[mat[i][j] - '0'].push_back(j);
         }
         for (int k = 0; k < 10; k++) {
-            if (min_row[k] != -1) {
-                int t;
-                if (min_col[k] != -1) {
-                    t = (i - min_row[k]) * (n - 1 - min_col[k]);
-                    res[k] = max(res[k], t);
-                }
-                if (max_col[k] != -1) {
-                    t = (i - min_row[k]) * max_col[k];
-                    res[k] = max(res[k], t);
+            if (vec[k].size() >= 1) {
+                int width = max(vec[k][vec[k].size() - 1], n - 1 - vec[k][0]);
+                int height = max(i - min_row[k], max_row[k] - i);
+                res[k] = max(res[k], width * height);
+                if (vec[k].size() > 1) {
+                    width = vec[k][vec[k].size() - 1] - vec[k][0];
+                    height = max(i, n - 1 - i);
+                    res[k] = max(res[k], width * height);
                 }
             }
         }
     }
-    vector<int> min_col(10, -1);
+    // by col
     for (int j = 0; j < n; j++) {
-        vector<int> mn_row(10, -1), mx_row(10, -1);
+        map<int, vector<int>> vec;
         for (int i = 0; i < n; i++) {
-            int c = mat[i][j] - '0';
-            if (mn_row[c] == -1) mn_row[c] = i;
-            if (min_col[c] == -1) min_col[c] = j;
-            mx_row[c] = j;
+            vec[mat[i][j] - '0'].push_back(i);
         }
         for (int k = 0; k < 10; k++) {
-            if (min_col[k] != -1) {
-                int t;
-                if (mn_row[k] != -1) {
-                    t = (j - min_col[k]) * (n - 1 - mn_row[k]);
-                    res[k] = max(res[k], t);
-                }
-                if (mx_row[k] != -1) {
-                    t = (j - min_col[k]) * mx_row[k];
-                    res[k] = max(res[k], t);
+            if (vec[k].size() >= 1) {
+                int width = max(vec[k][vec[k].size() - 1], n - 1 - vec[k][0]);
+                int height = max(j - min_col[k], max_col[k] - j);
+                res[k] = max(res[k], width * height);
+                if (vec[k].size() > 1) {
+                    width = vec[k][vec[k].size() - 1] - vec[k][0];
+                    height = max(j, n - 1 - j);
+                    res[k] = max(res[k], width * height);
                 }
             }
         }
     }
+
     for (int k = 0; k < 10; k++) {
         cout << res[k] << " ";
     }
