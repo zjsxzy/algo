097bf3064618089eda48234b8685fc4fd1f7e00f
zhangyang <zjsxzy@gmail.com>
2021-01-27 21:04:14 +0800

    cut vertex update

--- CodeTemplate/GraphTheory/FindCutVertex.cpp
+++ CodeTemplate/GraphTheory/FindCutVertex.cpp
@@ -7,22 +7,33 @@ void Tarjan(int u, int dep) {
         int v = graph[u][i].first;
         int id = graph[u][i].second;
         if (dfn[v] == -1) {
-            visit[id] = true;//It's an undirected graph
+            visited[id] = true;//It's an undirected graph
             Tarjan(v, dep + 1);
             low[u] = min(low[u], low[v]);
             if (low[v] >= dfn[u]) tag[u]++;//Vertex u is a Cut-Vertex
-        } else if (!visit[id]) {
+        } else if (!visited[id]) {
             low[u] = min(low[u], dfn[v]);
         }
     }
 }
 
 void FindCutVertex() {
-	for (int i = 0; i < N; i++)
+    for (int i = 0; i < edges; i++) {
+        visited[i] = false;
+    }
+	for (int i = 0; i < n; i++) {
 		dfn[i] = -1;
-	for (int i = 0; i < N; i++)
+        tag[i] = 0;
+    }
+	for (int i = 0; i < n; i++) {
 		if (dfn[i] == -1) {
 			Tarjan(i, 0);
 			tag[i]--;//Vertex i is the root
 		}
+    }
+    for (int i = 0; i < n; i++) {
+        if (tag[i]) {
+            // i is a cut vertex
+        }
+    }
 }
