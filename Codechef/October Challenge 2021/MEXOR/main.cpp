7eef694292d4f4e3497d56fa029e4e4d28e64c94
zjsxzy <zjsxzy@gmail.com>
2021-10-05 12:21:17 +0800

    mexor

---
+++ Codechef/October Challenge 2021/MEXOR/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+void solve() {
+    int x;
+    cin >> x;
+    int i = 0;
+    while ((i | (i + 1)) <= x) {
+        i = i | (i + 1);
+    }
+    cout << i + 1 << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    int ts;
+    cin >> ts;
+    while (ts--) {
+        solve();
+    }
+    return 0;
+}
+
