81fe04d2c6a75c9117ba4625c7297646c1821372
zjsxzy <zjsxzy@gmail.com>
2021-11-11 20:35:52 +0800

    makepal

---
+++ Codechef/November Challenge 2021/MAKEPAL/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+void solve() {
+    int n, cnt = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x & 1) cnt++;
+    }
+    cout << cnt / 2 << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    int ts;
+    cin >> ts;
+    while (ts--) {
+        solve();
+    }
+    return 0;
+}
+
