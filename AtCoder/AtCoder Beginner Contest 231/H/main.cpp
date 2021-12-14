da73a4dd331cc38b64921ee07fe9ffd936b2d61c
Zhang Yang <zjsxzy@gmail.com>
2021-12-14 10:50:29 +0800

    update h

--- AtCoder/AtCoder Beginner Contest 231/H/main.cpp
+++ AtCoder/AtCoder Beginner Contest 231/H/main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 #define abs(x) ((x) > 0 ? (x) : -(x))
 typedef long long LL;
 
-#define maxV 1005
+#define maxV 2005
 #define maxE 100 * maxV
-const int inf = 0x3f3f3f3f;
+const LL inf = (1LL <<60);
 struct Nod {
     int b, nxt;
-    int cap, cst;
-    void init(int b, int nxt, int cap,int cst) {
+    LL cap, cst;
+    void init(int b, int nxt, LL cap, LL cst) {
         this->b = b;
         this->nxt = nxt;
         this->cap = cap;
@@ -28,14 +28,17 @@ struct MinCost {
         memset(E, 255, sizeof(E));
         len = 0;
     }
-    void addCap(int a, int b, int cap, int cst) {
+    void addCap(int a, int b, LL cap, LL cst) {
         buf[len].init(b, E[a], cap, cst);   E[a] = len ++;
         buf[len].init(a, E[b], 0,  -cst);   E[b] = len ++;
     }
     bool spfa(int source, int sink) {
         static queue<int> q;
-        static int d[maxV];
-        memset(d,  63, sizeof(d));
+        static LL d[maxV];
+        // memset(d,  63, sizeof(d));
+        for (int i = 0; i < n; i++) {
+            d[i] = inf;
+        }
         memset(p, 255, sizeof(p));
 
         d[source] = 0;
@@ -55,10 +58,10 @@ struct MinCost {
         }
         return d[sink] != inf;
     }
-    pair<int, int> solve(int source, int sink) {
-        int minCost = 0,maxFlow = 0;
+    pair<LL, LL> solve(int source, int sink) {
+        LL minCost = 0, maxFlow = 0;
         while(spfa(source, sink)) {
-            int neck = inf;
+            LL neck = inf;
             for(int t=p[sink]; t != -1; t = p[ buf[t^1].b ])
                 neck = min(neck, buf[t].cap);
             maxFlow += neck;
@@ -75,8 +78,8 @@ struct MinCost {
 void solve() {
     int h, w, n;
     cin >> h >> w >> n;
-    mcmf.init(h + w + 2);
-    int source = 0, sink = h + w + 1;
+    int source = 0, sink = h + w + 1, cen = sink + 1;
+    mcmf.init(cen + 1);
     for (int i = 1; i <= h; i++) {
         mcmf.addCap(source, i, 1, 0);
     }
@@ -87,9 +90,13 @@ void solve() {
         int x, y, c;
         cin >> x >> y >> c;
         mcmf.addCap(x, h + y, 1, c);
+        mcmf.addCap(cen, h + y, 1, c);
+        mcmf.addCap(x, cen, 1, c);
     }
+    if (w > h) mcmf.addCap(source, cen, w - h, 0);
+    else mcmf.addCap(cen, sink, h - w, 0);
     auto res = mcmf.solve(source, sink);
-    cout << res.first << endl;
+    // cout << res.first << endl;
     cout << res.second << endl;
 }
 
