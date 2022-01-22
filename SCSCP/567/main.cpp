8f67487454ff855a4ab6750419544518624bbd7d
zjsxzy <zjsxzy@gmail.com>
2022-01-22 17:24:59 +0800

    567

--- SCSCP/567/main.cpp
+++ SCSCP/567/main.cpp
@@ -6,41 +6,78 @@ using namespace std;
 typedef long long LL;
 
 const int MAXN = 3000 + 5;
-int n, m, res;
-bool row[MAXN], col[MAXN];
-string grid[MAXN], curr[MAXN];
-
-void dfs(int r, int c, int step) {
-    if (r == n) return;
-    if (c == m) return;
-    if (step >= res) return;
-    bool flag = true;
-    for (int i = 0; i < n; i++) {
-        if (curr[i] != grid[i]) {
-            flag = false;
-            break;
-        }
-    }
-    if (flag) {
-        res = min(res, step);
-        return;
-    }
-    // flip row r
-    for (int j = 0; j < m; j++) {
-        curr[r][j] = 'R';
-    }
-    // flip column c
-}
+string grid[MAXN];
 
 void solve() {
+    int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         cin >> grid[i];
-        string str(m, '*');
-        curr[i] = str;
     }
-    res = INT_MAX;
-    dfs(0, 0, 0);
+    int res = 0, all = n * m;
+    while (all) {
+        int mx_row = 0, row = -1;
+        for (int i = 0; i < n; i++) {
+            bool flag = true;
+            int r = 0, b = 0;
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 'R') r++;
+                else if (grid[i][j] == 'B') b++;
+                if (r && b) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                if (r + b > mx_row) {
+                    row = i;
+                    mx_row = r + b;
+                }
+            }
+        }
+
+        int mx_col = 0, col = -1;
+        for (int j = 0; j < m; j++) {
+            bool flag = true;
+            int r = 0, b = 0;
+            for (int i = 0; i < n; i++) {
+                if (grid[i][j] == 'R') r++;
+                else if (grid[i][j] == 'B') b++;
+                if (r && b) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                if (r + b > mx_col) {
+                    col = j;
+                    mx_col = r + b;
+                }
+            }
+        }
+        if (mx_row == 0 && mx_col == 0) {
+            cout << -1 << endl;
+            return;
+        }
+        res++;
+        if (mx_row > mx_col) {
+            cout << row << endl;
+            for (int j = 0; j < m; j++) {
+                if (grid[row][j] != '*') {
+                    grid[row][j] = '*';
+                }
+            }
+            all += mx_row;
+        } else {
+            cout << col << endl;
+            for (int i = 0; i < n; i++) {
+                if (grid[i][col] != '*') {
+                    grid[i][col] = '*';
+                }
+            }
+            all += mx_col;
+        }
+    }
     cout << res << endl;
 }
 
