2b18a093aa13414c4097773227c47c486753812c
zjsxzy <zjsxzy@gmail.com>
2022-03-05 00:06:13 +0800

    update

--- Codeforces/Round774-Div2/C/main.cpp
+++ Codeforces/Round774-Div2/C/main.cpp
@@ -6,48 +6,6 @@ using namespace std;
 typedef long long LL;
 
 void solve() {
-    map<LL, int> dist;
-    vector<LL> val;
-    queue<LL> q;
-    for (int i = 0; i < 60; i++) {
-        if ((1LL << i) > 1e12) break;
-        q.push(1LL << i);
-        val.push_back(1LL << i);
-        dist[1LL << i] = 1;
-    }
-    LL prod = 1;
-    for (int i = 2; i <= 20; i++) {
-        prod *= i;
-        if (prod > 1e12) break;
-        q.push(prod);
-        val.push_back(prod);
-        dist[prod] = 1;
-    }
-    cout << val.size() << endl;
-    return;
-    while (!q.empty()) {
-        LL t = q.front(); q.pop();
-        cout << t << " " << dist[t] << endl;
-        for (auto& v: val) {
-            LL tt = t + v;
-            if (tt > 1e12) continue;
-            if (dist.find(tt) == dist.end()) {
-                dist[tt] = dist[t] + 1;
-                q.push(tt);
-            }
-        }
-    }
-    int ts;
-    cin >> ts;
-    while (ts--) {
-        LL n;
-        cin >> n;
-        if (dist.find(n) == dist.end()) {
-            cout << -1 << endl;
-        } else {
-            cout << dist[n] << endl;
-        }
-    }
 }
 
 int main() {
