f5184fcee4a878aa36f4eee7ce65ec09d0a53d9f
zjsxzy <zjsxzy@gmail.com>
2022-05-01 21:50:49 +0800

    update squary

--- Google/Google Code Jam 2022/Round 1C/Squary/main.cpp
+++ Google/Google Code Jam 2022/Round 1C/Squary/main.cpp
@@ -8,32 +8,43 @@ typedef long long LL;
 void solve() {
     int n, k;
     cin >> n >> k;
-    LL sum = 0, sumsq = 0;
-    for (int i = 0; i < n; i++) {
-        LL x;
+    vector<int> a(n);
+    LL sum = 0, sumsp = 0;
+    for (auto& x: a) {
         cin >> x;
         sum += x;
-        sumsq += x * x;
     }
-    if (sum == 0) {
-        if (sumsq != 0) {
-            cout << "IMPOSSIBLE" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            sumsp += 1ll * a[i] * a[j];
+        }
+    }
+    LL inf = 1ll << 60;
+    vector<LL> ans(k, inf);
+    if (k == 1) {
+        if (sum == 0) {
+            if (sumsp == 0) ans[0] = 0;
         } else {
-            cout << 0 << endl;
+            if (abs(sumsp) % abs(sum) == 0) ans[0] = -sumsp / sum;
         }
     } else {
-        if ((sumsq - sum * sum) % (2 * sum) == 0) {
-            cout << (sumsq - sum * sum) / (2 * sum) << endl;
-        } else {
-            cout << "IMPOSSIBLE" << endl;
+        ans[0] = -sum + 1;
+        ans[1] = -sum + (sum * sum - sumsp);
+        for (int i = 2; i < k; i++) {
+            ans[i] = 0;
         }
+    }
+    if (ans == vector<LL>(k, inf)) {
+        cout << "IMPOSSIBLE" << endl;
+    } else {
+        for (int i = 0; i < k; i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
     }
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-#endif
     int ts;
     cin >> ts;
     for (int i = 1; i <= ts; i++) {
