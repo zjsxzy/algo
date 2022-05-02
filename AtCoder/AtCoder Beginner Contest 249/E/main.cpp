89cf4e65da6072adab375e261d76f5345e5d491a
zjsxzy <zjsxzy@gmail.com>
2022-05-02 11:30:35 +0800

    update e

---
+++ AtCoder/AtCoder Beginner Contest 249/E/main.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+const int maxn = 3005;
+int n, mod;
+struct Mod_Int {
+    int x;
+    Mod_Int() : x(0) {}
+    Mod_Int(long long y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    static int get_mod() { return mod; }
+    Mod_Int &operator+=(const Mod_Int &p) {
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator-=(const Mod_Int &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator*=(const Mod_Int &p) {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+    Mod_Int &operator/=(const Mod_Int &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+    Mod_Int &operator++() { return *this += Mod_Int(1); }
+    Mod_Int operator++(int) {
+        Mod_Int tmp = *this;
+        ++*this;
+        return tmp;
+    }
+    Mod_Int &operator--() { return *this -= Mod_Int(1); }
+    Mod_Int operator--(int) {
+        Mod_Int tmp = *this;
+        --*this;
+        return tmp;
+    }
+    Mod_Int operator-() const { return Mod_Int(-x); }
+    Mod_Int operator+(const Mod_Int &p) const { return Mod_Int(*this) += p; }
+    Mod_Int operator-(const Mod_Int &p) const { return Mod_Int(*this) -= p; }
+    Mod_Int operator*(const Mod_Int &p) const { return Mod_Int(*this) *= p; }
+    Mod_Int operator/(const Mod_Int &p) const { return Mod_Int(*this) /= p; }
+    bool operator==(const Mod_Int &p) const { return x == p.x; }
+    bool operator!=(const Mod_Int &p) const { return x != p.x; }
+    Mod_Int inverse() const {
+        assert(*this != Mod_Int(0));
+        return pow(mod - 2);
+    }
+    Mod_Int pow(long long k) const {
+        Mod_Int now = *this, ret = 1;
+        for (; k > 0; k >>= 1, now *= now) {
+            if (k & 1) ret *= now;
+        }
+        return ret;
+    }
+    friend ostream &operator<<(ostream &os, const Mod_Int &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, Mod_Int &p) {
+        long long a;
+        is >> a;
+        p = Mod_Int(a);
+        return is;
+    }
+};
+Mod_Int dp[maxn][maxn], dpsum[maxn][maxn];
+
+void solve() {
+    cin >> n >> mod;
+    for (int len = 1; len <= n; len++) {
+        int d = to_string(len).size();
+        if (d + 1 <= n) dp[d + 1][len] = 26;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            for (int l = 1, r = 10, d = 1; l <= j && d < i; l *= 10, r *= 10, d++) {
+                dp[i][j] += (dpsum[i - d - 1][j - l + 1] -
+                             dpsum[i - d - 1][max(0, j - r + 1)]) * 25;
+            }
+        }
+        for (int j = 0; j <= n; j++) {
+            dpsum[i][j + 1] = dpsum[i][j] + dp[i][j];
+        }
+    }
+
+    Mod_Int res = 0;
+    for (int i = 0; i < n; i++) {
+        res += dp[i][n];
+    }
+    cout << res << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+
