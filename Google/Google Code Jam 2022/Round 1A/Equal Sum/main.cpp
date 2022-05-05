5945a1aaa3c77c26647c8c73cf4e07895718aa16
zjsxzy <zjsxzy@gmail.com>
2022-05-05 20:35:12 +0800

    update equal sum

--- Google/Google Code Jam 2022/Round 1A/Equal Sum/main.cpp
+++ Google/Google Code Jam 2022/Round 1A/Equal Sum/main.cpp
@@ -1,57 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define PB push_back
-#define SZ(v) ((int)(v).size())
-#define abs(x) ((x) > 0 ? (x) : -(x))
 typedef long long LL;
-
-bool can;
-LL sum;
-map<pair<int, LL>, bool> dp;
-vector<int> ans;
-void dfs(int m, LL n, vector<int>& nums) {
-    if (n > sum) return;
-    if (can == true) return;
-    if (m == nums.size()) {
-        if(n == sum / 2) {
-            can = true;
-            for (auto& x: ans) cout << x << " ";
-            cout << endl;
-            return;
-        }
-    } else{
-        if (dp.find({m, n}) != dp.end()) return;
-        dp[{m, n}] = true;
-        dfs(m + 1, n, nums);
-        ans.push_back(nums[m]);
-        dfs(m + 1, n + (LL)nums[m], nums);
-        ans.pop_back();
-    }
-}
 
 void solve() {
     int n;
     cin >> n;
-    vector<int> a(n);
-    LL suma = 0, sumb = 0;
-    for (int i = 0; i < n; i++) {
-        a[i] = i + 1;
-        suma += a[i];
-        cout << i + 1;
-        if (i == n - 1) cout << endl;
-        else cout << " ";
+    LL sum = 0;
+    for (int i = 0; i < min(n, 30); i++) {
+        sum += 1 << i;
+        cout << (1 << i) << " ";
     }
-    vector<int> b(n);
-    for (int i = 0; i < n; i++) {
-        cin >> b[i];
-        sumb += (LL)b[i];
-        a.push_back(b[i]);
+    vector<int> a;
+    for (int i = 0; i < n - 30; i++) {
+        a.push_back(1000000000 - i);
+        sum += 1000000000 - i;
+        std::cout << 1000000000 - i << " ";
     }
-    sum = suma + sumb;
-    ans.clear();
-    dp.clear();
-    can = false;
-    dfs(0, 0, a);
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        a.push_back(x);
+        sum += x;
+    }
+    sum /= 2;
+    vector<int> ans;
+    for (auto x: a) {
+        if (sum >= x) {
+            ans.push_back(x);
+            sum -= x;
+        }
+    }
+    for (int i = 29; i >= 0; i--) {
+        if (sum >> i & 1) {
+            sum -= 1 << i;
+            ans.push_back(1 << i);
+        }
+    }
+    for (auto x: ans) {
+        cout << x << " ";
+    }
+    cout << endl;
 }
 
 int main() {
