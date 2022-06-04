07d32f63a0878840bfcd06fe98ff486b6e4d5e1f
zjsxzy <zjsxzy@gmail.com>
2022-06-05 00:17:10 +0800

    update segtree

--- CodeTemplate/DataStucture/SegTree.cpp
+++ CodeTemplate/DataStucture/SegTree.cpp
@@ -1,3 +1,42 @@
+// 左闭右开区间，E是默认值
+int f(int a, int b){
+  return max(a, b);
+}
+template <typename T>
+struct segment_tree{
+	int N;
+	vector<T> ST;
+	function<T(T, T)> f;
+	T E;
+	segment_tree(vector<T> A, function<T(T, T)> f, T E): f(f), E(E){
+		int n = A.size();
+		N = 1;
+		while (N < n){
+			N *= 2;
+		}
+		ST = vector<T>(N * 2 - 1, E);
+		for (int i = 0; i < n; i++){
+			ST[N - 1 + i] = A[i];
+		}
+		for (int i = N - 2; i >= 0; i--){
+			ST[i] = f(ST[i * 2 + 1], ST[i * 2 + 2]);
+		}
+	}
+	T query(int L, int R, int i, int l, int r){
+		if (R <= l || r <= L){
+			return E;
+		} else if (L <= l && r <= R){
+			return ST[i];
+		} else {
+			int m = (l + r) / 2;
+			return f(query(L, R, i * 2 + 1, l, m), query(L, R, i * 2 + 2, m, r));
+		}
+	}
+	T query(int L, int R){
+		return query(L, R, 0, 0, N);
+	}
+};
+
 const int MAXN = 100005;
 const int inf = 1e9;
 int n;
