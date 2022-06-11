a63440197ae08530455c826f41a8eb4c69c2180d
zjsxzy <zjsxzy@gmail.com>
2022-06-11 23:52:47 +0800

    update e

--- AtCoder/AtCoder Beginner Contest 255/E/main.cpp
+++ AtCoder/AtCoder Beginner Contest 255/E/main.cpp
@@ -5,14 +5,25 @@ typedef long long LL;
 void solve() {
     int n, m;
     cin >> n >> m;
-    vector<int> s(n - 1);
+    vector<LL> s(n - 1);
     for (int i = 0; i < n - 1; i++) cin >> s[i];
-    vector<int> x(m);
+    vector<LL> x(m);
     for (int i = 0; i < m; i++) cin >> x[i];
-    vector<int> a(n);
+    vector<LL> a(n);
     for (int i = 1; i < n; i++) {
         a[i] = s[i - 1] - a[i - 1];
     }
+    map<LL, int> cnt;
+    int mul = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cnt[(a[i] - x[j]) * mul]++;
+        }
+        mul *= -1;
+    }
+    int res = 0;
+    for (auto [_, c]: cnt) res = max(res, c);
+    cout << res << endl;
 }
 
 int main() {
