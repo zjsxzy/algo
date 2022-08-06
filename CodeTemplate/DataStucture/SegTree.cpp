4e01918b3aa4d23ad482902955a95e039dec04d6
zhangyang <zjsxzy@gmail.com>
2022-08-06 23:26:26 +0800

    update seg tree

--- CodeTemplate/DataStucture/SegTree.cpp
+++ CodeTemplate/DataStucture/SegTree.cpp
@@ -1,3 +1,52 @@
+struct SegmentTree {
+    using T = int;
+
+    int N;
+    vector<T> dat;
+
+    T id = 0;
+    T F(T &a, T &b) { return a + b; }
+
+    SegmentTree(int n){
+        N = 1;
+        while(n > N) N = N << 1;
+        dat = vector<T>(2 * N - 1, id);
+    }
+
+    SegmentTree(int n, vector<T> &v){
+        N = 1;
+        while(n > N) N = N << 1;
+        dat = vector<T>(2 * N - 1, id);
+        for(int i = 0; i < n; i++) dat[i + N - 1] = v[i];
+        for(int i = N - 2; i >= 0; i--) dat[i] = F(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    SegmentTree(){}
+
+    void update(int k, T a){
+        k += N - 1;
+        dat[k] = a;
+        while(k > 0){
+            k = (k - 1) / 2;
+            dat[k] = F(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+
+    void reset() { fill(dat.begin(), dat.end(), id); }
+
+    T get(int a, int b, int k, int l, int r){
+        if(r <= a || b <= l) return id;
+        if(a <= l && r <= b) return dat[k];
+        else{
+            T vl = get(a, b, k * 2 + 1, l, (l + r) / 2);
+            T vr = get(a, b, k * 2 + 2, (l + r) / 2, r);
+            return F(vl, vr);
+        }
+    }
+    T get(int a, int b) { return get(a, b, 0, 0, N); }
+
+    T val(int k){ return dat[k + N - 1]; }
+};
 // 左闭右开区间，E是默认值
 int f(int a, int b) {
   return max(a, b);
