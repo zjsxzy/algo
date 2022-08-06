83740388447f466f4b8964cd2c23e73cf173ae89
zhangyang <zjsxzy@gmail.com>
2022-08-06 22:00:46 +0800

    upd bit

--- CodeTemplate/DataStucture/BinaryIndexTree.cpp
+++ CodeTemplate/DataStucture/BinaryIndexTree.cpp
@@ -1,3 +1,58 @@
+template <typename T>
+struct Binary_Indexed_Tree {
+    vector<T> bit;
+    const int n;
+
+    Binary_Indexed_Tree(const vector<T> &v) : n((int)v.size()) {
+        bit.resize(n + 1);
+        copy(begin(v), end(v), begin(bit) + 1);
+        for (int a = 2; a <= n; a <<= 1) {
+            for (int b = a; b <= n; b += a) bit[b] += bit[b - a / 2];
+        }
+    }
+
+    Binary_Indexed_Tree(int n, const T &x) : n(n) {
+        bit.resize(n + 1);
+        vector<T> v(n, x);
+        copy(begin(v), end(v), begin(bit) + 1);
+        for (int a = 2; a <= n; a <<= 1) {
+            for (int b = a; b <= n; b += a) bit[b] += bit[b - a / 2];
+        }
+    }
+
+    void add(int i, const T &x) {
+        for (i++; i <= n; i += (i & -i)) bit[i] += x;
+    }
+
+    void change(int i, const T &x) { add(i, x - query(i, i + 1)); }
+
+    T sum(int i) const {
+        T ret = 0;
+        for (; i > 0; i -= (i & -i)) ret += bit[i];
+        return ret;
+    }
+
+    T query(int l, int r) const { return sum(r) - sum(l); }
+
+    T operator[](int i) const { return query(i, i + 1); }
+
+    int lower_bound(T x) const { // v[0]+...+v[r] >= x
+        int ret = 0;
+        for (int k = 31 - __builtin_clz(n); k >= 0; k--) {
+            if (ret + (1 << k) <= n && bit[ret + (1 << k)] < x) x -= bit[ret += (1 << k)];
+        }
+        return ret;
+    }
+
+    int upper_bound(T x) const { // v[0]+...+v[r] > x
+        int ret = 0;
+        for (int k = 31 - __builtin_clz(n); k >= 0; k--) {
+            if (ret + (1 << k) <= n && bit[ret + (1 << k)] <= x) x -= bit[ret += (1 << k)];
+        }
+        return ret;
+    }
+};
+
 /*
  * One dimension Binary Index Tree. Notice that the array is C[1], C[2], ..., C[N]
  */
