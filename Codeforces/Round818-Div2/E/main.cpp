c62b998780f38bf2a4142f28e3cedce7e6fc5747
zhangyang <zjsxzy@gmail.com>
2022-09-03 15:25:58 +0800

    add e

---
+++ Codeforces/Round818-Div2/E/main.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+const int MOD = 1e9 + 7;
+template <int mod>
+struct Mod_Int {
+    int x;
+    Mod_Int() : x(0) {}
+    Mod_Int(long long y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    static int get_mod() { return mod; }
+    Mod_Int &operator+=(const Mod_Int &p) {
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator-=(const Mod_Int &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator*=(const Mod_Int &p) {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+    Mod_Int &operator/=(const Mod_Int &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+    Mod_Int &operator++() { return *this += Mod_Int(1); }
+    Mod_Int operator++(int) {
+        Mod_Int tmp = *this;
+        ++*this;
+        return tmp;
+    }
+    Mod_Int &operator--() { return *this -= Mod_Int(1); }
+    Mod_Int operator--(int) {
+        Mod_Int tmp = *this;
+        --*this;
+        return tmp;
+    }
+    Mod_Int operator-() const { return Mod_Int(-x); }
+    Mod_Int operator+(const Mod_Int &p) const { return Mod_Int(*this) += p; }
+    Mod_Int operator-(const Mod_Int &p) const { return Mod_Int(*this) -= p; }
+    Mod_Int operator*(const Mod_Int &p) const { return Mod_Int(*this) *= p; }
+    Mod_Int operator/(const Mod_Int &p) const { return Mod_Int(*this) /= p; }
+    bool operator==(const Mod_Int &p) const { return x == p.x; }
+    bool operator!=(const Mod_Int &p) const { return x != p.x; }
+    Mod_Int inverse() const {
+        assert(*this != Mod_Int(0));
+        return pow(mod - 2);
+    }
+    Mod_Int pow(long long k) const {
+        Mod_Int now = *this, ret = 1;
+        for (; k > 0; k >>= 1, now *= now) {
+            if (k & 1) ret *= now;
+        }
+        return ret;
+    }
+    friend ostream &operator<<(ostream &os, const Mod_Int &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, Mod_Int &p) {
+        long long a;
+        is >> a;
+        p = Mod_Int<mod>(a);
+        return is;
+    }
+};
+using mint = Mod_Int<MOD>;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<int>> divisors(n + 1);
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j += i) {
+            divisors[j].push_back(i);
+        }
+    }
+    vector<int> phi(n + 1);
+    iota(phi.begin(), phi.end(), 0);
+    phi[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        if ((int)divisors[i].size() == 2) {
+            for (int j = i; j <= n; j += i) {
+                phi[j] /= i;
+                phi[j] *= i - 1;
+            }
+        }
+    }
+    mint res = 0;
+    for (int c = 1; c <= n; c++) {
+        int s = n - c;
+        for (auto& d: divisors[s]) {
+            int t = 1ll * c * d / gcd(c, d) % MOD;
+            res += mint(t) * mint(phi[s / d]);
+        }
+    }
+    cout << res << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+   freopen("in.txt", "r", stdin);
+#endif
+   int ts = 1;
+   // cin >> ts;
+   for (int t = 1; t <= ts; t++) {
+       solve();
+   }
+   return 0;
+}
+
