d3f8d96cf5c2e9bd04eb6257bc1a2ebd6d52be33
zhangyang <zjsxzy@gmail.com>
2022-09-03 15:26:24 +0800

    upd euler

--- CodeTemplate/Math/EulerFunction.cpp
+++ CodeTemplate/Math/EulerFunction.cpp
@@ -14,3 +14,23 @@ int Euler(int n) {
 	if (n > 1) ret *= n - 1;
 	return ret;
 }
+
+void euler(int n) {
+    vector<vector<int>> divisors(n + 1);
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j += i) {
+            divisors[j].push_back(i);
+        }
+    }
+    vector<int> phi(n + 1);
+    iota(phi.begin(), phi.end(), 0);
+    phi[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        if ((int)divisors[i].size() == 2) {
+            for (int j = i; j <= n; j += i) {
+                phi[j] /= i;
+                phi[j] *= i - 1;
+            }
+        }
+    }
+}
