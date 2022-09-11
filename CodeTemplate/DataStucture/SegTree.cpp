8690cbc3e0ad5ab0265b62745ef4c7329221e062
zhangyang <zjsxzy@gmail.com>
2022-09-11 13:34:39 +0800

    update segtree

--- CodeTemplate/DataStucture/SegTree.cpp
+++ CodeTemplate/DataStucture/SegTree.cpp
@@ -137,6 +137,14 @@ template <class S, S (*op)(S, S), S (*e)()> struct segtree {
     void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
 };
 }  // namespace atcoder
+using namespace atcoder;
+int op(int a, int b) {
+    return max(a, b);
+}
+int e() {
+    return 0;
+}
+segtree<int, op, e> tree(m);
 
 struct SegmentTree {
     using T = int;
