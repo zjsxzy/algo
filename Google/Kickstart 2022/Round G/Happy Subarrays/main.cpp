5fb8ffeab5cc065289828f46c2cc07a5c08960f8
Zhang Yang <zjsxzy@gmail.com>
2022-10-15 23:16:51 +0800

    upd

--- Google/Kickstart 2022/Round G/Happy Subarrays/main.cpp
+++ Google/Kickstart 2022/Round G/Happy Subarrays/main.cpp
@@ -3,12 +3,33 @@ using namespace std;
 typedef long long LL;
 
 void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& x: a) cin >> x;
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int sum = 0;
+            bool flag = true;
+            for (int k = i; k <= j; k++) {
+                sum += a[k];
+                if (sum < 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) res += sum;
+        }
+    }
+    cout << res << endl;
 }
 
 int main() {
    int ts = 1;
    cin >> ts;
    for (int t = 1; t <= ts; t++) {
+       cout << "Case #" << t << ": ";
        solve();
    }
    return 0;
