f5d9267ba70331bdb9c43500d4abdc59babf8046
zjsxzy <zjsxzy@gmail.com>
2022-10-26 13:22:03 +0800

    upd

--- AtCoder/AtCoder Beginner Contest 221/E/main.cpp
+++ AtCoder/AtCoder Beginner Contest 221/E/main.cpp
@@ -93,15 +93,15 @@ struct Fenwick{
 };
 
 void compress(vector<int>& as) {
-	vector<int> num;
-	for (int i = 0; i < (int)as.size(); i++) {
-		num.push_back(as[i]);
-	}
-	sort(num.begin(), num.end());
+    vector<int> num;
+    for (int i = 0; i < (int)as.size(); i++) {
+        num.push_back(as[i]);
+    }
+    sort(num.begin(), num.end());
     num.resize(unique(num.begin(), num.end()) - num.begin());
-	for (int i = 0; i < (int)as.size(); i++) {
-		as[i] = lower_bound(num.begin(), num.end(), as[i]) - num.begin() + 1;
-	}
+    for (int i = 0; i < (int)as.size(); i++) {
+        as[i] = lower_bound(num.begin(), num.end(), as[i]) - num.begin() + 1;
+    }
 }
 
 void solve() {
