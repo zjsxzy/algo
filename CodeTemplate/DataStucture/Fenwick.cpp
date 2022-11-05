28b943991fbf40c294551434674c411317c973f7
zhangyang <zjsxzy@gmail.com>
2022-11-05 21:37:15 +0800

    upd fenwick

--- CodeTemplate/DataStucture/Fenwick.cpp
+++ CodeTemplate/DataStucture/Fenwick.cpp
@@ -1,25 +1,26 @@
 // index from 0
 struct Fenwick{
-    vector<int> tree;
+    using T = int;
+    vector<T> tree;
     int n;
     Fenwick() {}
     Fenwick(int _n) {
         n = _n;
         tree.assign(n, 0);
     }
-    void upd(int pos, int val) {
+    void upd(int pos, T val) {
         for (; pos < n; pos |= pos + 1) {
             tree[pos] += val;
         }
     }
-    int get(int r) {
-        int ans = 0;
+    T get(int r) {
+        T ans = 0;
         for (; r >= 0; r = (r & (r + 1)) - 1) {
             ans += tree[r];
         }
         return ans;
     }
-    int get(int l, int r) {
+    T get(int l, int r) {
         if (l > r) return 0;
         return get(r) - get(l - 1);
     }
