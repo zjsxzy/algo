a58a40e24d824071724748e8a59f7ca13c67b6d8
zhangyang <zjsxzy@gmail.com>
2022-11-29 12:57:01 +0800

    upd uf

--- CodeTemplate/DataStucture/UnionFind.cpp
+++ CodeTemplate/DataStucture/UnionFind.cpp
@@ -24,6 +24,27 @@ struct union_find{
     }
 };
 
+// directional dsu
+struct DSU {
+    std::vector<int> f, siz;
+    DSU(int n) : f(n), siz(n, 1) { std::iota(f.begin(), f.end(), 0); }
+    int leader(int x) {
+        while (x != f[x]) x = f[x] = f[f[x]];
+        return x;
+    }
+    bool same(int x, int y) { return leader(x) == leader(y); }
+    // merge y into x
+    bool merge(int x, int y) {
+        x = leader(x);
+        y = leader(y);
+        if (x == y) return false;
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    int size(int x) { return siz[leader(x)]; }
+};
+
 // with solution
 struct union_find {
     int n;
