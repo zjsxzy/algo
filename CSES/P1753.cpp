005a23db7698e440024a548f4b91c08fb70b4122
zhangyang <zjsxzy@gmail.com>
2022-12-16 10:25:34 +0800

    add 1753 kmp

--- CSES/P1753.cpp
+++ CSES/P1753.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
 vector<int> find_substring(string pattern, string text) {
     int n = pattern.size();
     vector<int> next(n + 1, 0);
@@ -30,4 +34,20 @@ vector<int> find_substring(string pattern, string text) {
         }
     }
     return position;
+}
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+    vector<int> p = find_substring(t, s);
+    cout << (int)p.size() << endl;
+}
+
+int main() {
+    int ts = 1;
+    // cin >> ts;
+    for (int t = 1; t <= ts; t++) {
+        solve();
+    }
+    return 0;
 }
