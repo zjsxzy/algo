409112194fa78cfb5f2dde85dc1a06d6d6a72101
zhangyang <zjsxzy@gmail.com>
2023-03-05 19:28:19 +0800

    upd

--- USACO/Contests/2023 February/Silver/Cow-libi/main.cpp
+++ USACO/Contests/2023 February/Silver/Cow-libi/main.cpp
@@ -5,36 +5,41 @@ typedef long long LL;
 void solve() {
     int g, n;
     cin >> g >> n;
-    vector<int> vst(g);
     vector<tuple<int, int, int>> graze;
     for (int i = 0; i < g; i++) {
         int x, y, t;
         cin >> x >> y >> t;
-        graze.push_back(make_tuple(x, y, t));
+        graze.push_back(make_tuple(t, x, y));
     }
+    sort(graze.begin(), graze.end());
     vector<tuple<int, int, int>> cow;
     for (int i = 0; i < n; i++) {
         int x, y, t;
         cin >> x >> y >> t;
-        cow.push_back(make_tuple(x, y, t));
+        cow.push_back(make_tuple(t, x, y));
     }
     auto dist = [&](LL x1, LL y1, LL x2, LL y2) {
         return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
     };
+    auto reachable = [&](tuple<int, int, int> a, tuple<int, int, int> b) {
+        return dist(get<1>(a), get<2>(a), get<1>(b), get<2>(b)) <= (LL)(get<0>(a) - get<0>(b)) * (get<0>(a) - get<0>(b));
+    };
     int res = 0;
     for (int i = 0; i < n; i++) {
-        bool flag = true;
-        for (int j = 0; j < g; j++) {
-            if (vst[j]) continue;
-            LL dt = (LL)(get<2>(cow[i])) - get<2>(graze[j]);
-            LL t2 = dt * dt;
-            if (dist(get<0>(cow[i]), get<1>(cow[i]), get<0>(graze[j]), get<1>(graze[j])) <= t2) {
-                vst[j] = true;
-                flag = false;
-                break;
-            }
+        auto x = lower_bound(graze.begin(), graze.end(), cow[i]);
+        // if (x == graze.end()) x--;
+        // if (x != graze.begin()) x--;
+        cout << i << ' ' << get<1>(cow[i]) << ' ' << get<2>(cow[i]) << ' ' << get<0>(cow[i]) << endl;
+        cout << get<1>(*x) << ' ' << get<2>(*x) << ' ' << get<0>(*x) << endl;
+        bool flag = reachable(*x, cow[i]);
+        x++;
+        if (x != graze.end()) {
+            flag |= reachable(*x, cow[i]);
         }
-        res += flag;
+        // x++;
+        // if (x != graze.end()) flag |= reachable(*x, cow[i]);
+        // cout << i << ' ' << flag << endl;
+        res += !flag;
     }
     cout << res << endl;
 }
