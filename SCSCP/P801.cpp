76e90b14b813e5e18cb0e69c9c0efe9804079f6f
zhangyang <zjsxzy@gmail.com>
2023-05-04 19:36:50 +0800

    add 801

---
+++ SCSCP/P801.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+const int MOD = 998244353;
+template <int mod>
+struct Mod_Int {
+    int x;
+    Mod_Int() : x(0) {}
+    Mod_Int(long long y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    static int get_mod() { return mod; }
+    Mod_Int &operator+=(const Mod_Int &p) {
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator-=(const Mod_Int &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator*=(const Mod_Int &p) {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+    Mod_Int &operator/=(const Mod_Int &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+    Mod_Int &operator++() { return *this += Mod_Int(1); }
+    Mod_Int operator++(int) {
+        Mod_Int tmp = *this;
+        ++*this;
+        return tmp;
+    }
+    Mod_Int &operator--() { return *this -= Mod_Int(1); }
+    Mod_Int operator--(int) {
+        Mod_Int tmp = *this;
+        --*this;
+        return tmp;
+    }
+    Mod_Int operator-() const { return Mod_Int(-x); }
+    Mod_Int operator+(const Mod_Int &p) const { return Mod_Int(*this) += p; }
+    Mod_Int operator-(const Mod_Int &p) const { return Mod_Int(*this) -= p; }
+    Mod_Int operator*(const Mod_Int &p) const { return Mod_Int(*this) *= p; }
+    Mod_Int operator/(const Mod_Int &p) const { return Mod_Int(*this) /= p; }
+    bool operator==(const Mod_Int &p) const { return x == p.x; }
+    bool operator!=(const Mod_Int &p) const { return x != p.x; }
+    Mod_Int inverse() const {
+        assert(*this != Mod_Int(0));
+        return pow(mod - 2);
+    }
+    Mod_Int pow(long long k) const {
+        Mod_Int now = *this, ret = 1;
+        for (; k > 0; k >>= 1, now *= now) {
+            if (k & 1) ret *= now;
+        }
+        return ret;
+    }
+    friend ostream &operator<<(ostream &os, const Mod_Int &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, Mod_Int &p) {
+        long long a;
+        is >> a;
+        p = Mod_Int<mod>(a);
+        return is;
+    }
+};
+using mint = Mod_Int<MOD>;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (auto& x: a) cin >> x;
+    int k, x;
+    cin >> k >> x;
+    int tot = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= x) tot++;
+    }
+    vector<vector<mint>> comb(n + 1, vector<mint>(n + 1, 0));
+    for (int i = 0; i <= n; i++) {
+        comb[i][0] = 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            comb[i][j] = comb[i - 1][j - 1] + comb[i - 1][j];
+        }
+    }
+    mint res = 0;
+    if (tot < k) {
+        cout << 0 << endl;
+        return;
+    }
+    for (int i = k; i <= tot; i++) {
+        if (i > m) break;
+        res += comb[tot][i] * comb[n - tot][m - i];
+    }
+    cout << res << endl;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int ts = 1;
+    // cin >> ts;
+    for (int t = 1; t <= ts; t++) {
+        solve();
+    }
+    return 0;
+}
