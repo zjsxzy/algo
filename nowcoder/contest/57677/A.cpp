e2649e0c91e567eb0987a6098ae7a4b440c6d154
zhangyang <zjsxzy@gmail.com>
2023-05-19 22:57:47 +0800

    upd

--- nowcoder/contest/57677/A.cpp
+++ nowcoder/contest/57677/A.cpp
@@ -2,55 +2,37 @@
 using namespace std;
 typedef long long LL;
 
-bool prime(int x) {
-    for (int i = 2; i * i <= x; i++) {
-        if (x % i == 0) return false;
+const int maxn = 1e5 + 10;
+int prime[maxn], tot;
+bool vis[maxn];
+
+void init() {
+    for (int i = 2; i <= 1e5; i++) {
+        if (!vis[i]) {
+            prime[tot++] = i;
+            for (int j = i + i; j <= 1e5; j += i) {
+                vis[j] = 1;
+            }
+        }
     }
-    return true;
-}
-
-int pow_mod(int a, int i, int n) {
-	if (i == 0) return 1 % n;
-	int res = 1;
-	for (; i; i >>= 1) {
-		if (i & 1) res = (long long)res * a % n;
-		a = (long long)a * a % n;
-	}
-	return res;
-}
-
-bool test(int n, int a, int d) {
-	if (n == 2) return true;
-	if (n == a) return true;
-	if ((n & 1) == 0) return false;
-	while (!(d & 1)) d = d >> 1;
-	int t = pow_mod(a, d, n);
-	while ((d != n - 1) && (t != 1) && (t != n - 1)) {
-		t = (long long)t * t % n;
-		d = d << 1;
-	}
-	return (t == n - 1 || (d & 1) == 1);
 }
 
 bool isPrime(int n) {
-	if (n < 2) return false;
-	int a[] = {2, 3, 61}; //测试集，更广的测试范围需要更大的测试集
-	for (int i = 0; i < 3; i++) {
-		if (!test(n, a[i], n - 1)) return false;
- 	}
- 	return true;
+    for (int i = 0; i < tot && prime[i] * prime[i] <= n; i++) {
+        if (n % prime[i] == 0) return false;
+    }
+    return true;
 }
 
 void solve() {
     int n;
     cin >> n;
-    if (n == 4) {
-        cout << 4 << endl;
+    if (n <= 5) {
+        cout << n << endl;
         return;
     }
-    if (n % 2 == 0) n--;
     while (!isPrime(n)) {
-        n -= 2;
+        n--;
     }
     cout << n << endl;
 }
@@ -58,6 +40,7 @@ void solve() {
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
+    init();
     int ts = 1;
     cin >> ts;
     for (int t = 1; t <= ts; t++) {
--- nowcoder/contest/57677/C.cpp
+++ nowcoder/contest/57677/C.cpp
@@ -2,52 +2,24 @@
 using namespace std;
 typedef long long LL;
 
-int f[20][1 << 20], g[20][1 << 20];
-int vst[20];
-int e[20][20];
-int n;
-
-void dfs(int u, int p) {
-    vst[u] = true;
-    for (int v = 0; v < n; u++) {
-        if (!vst[v]) {
-            dfs(v, u);
-        } else if (v != p) {
-
-        }
-    }
-}
-
 void solve() {
+    int n;
     cin >> n;
     vector<int> a(n);
     for (auto& x: a) cin >> x;
-    for (int i = 0; i < n; i++) vst[i] = 0;
-    memset(e, 0, sizeof(e));
+    vector e(n, vector(n, 0));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> e[i][j];
         }
     }
-    for (int i = 0; i < n; i++) {
-        if (vst[i]) continue;
-        dfs(i, -1);
-    }
     LL res = 1e18;
     for (int st = 0; st < n; st++) {
-        if (vst[st]) continue;
-        for (int u = 0; u < n; u++) {
-            for (int s = 0; s < (1 << n); s++) {
-                f[u][s] = 1e9;
-                g[u][s] = 1e9;
-            }
-        }
-        f[st][1 << st] = 0;
-        g[st][1 << st] = 0;
+        vector dp(n, vector(1 << n, 1e9));
+        dp[st][0] = 0;
         for (int s = 1; s < (1 << n); s++) {
             for (int u = 0; u < n; u++) {
                 if (s >> u & 1) {
-                    if (f[u][s] == 1e9) continue;
                     LL nodes = f[u][s], edges = g[u][s];
                     for (int v = 0; v < n; v++) {
                         if (e[u][v] && !(s >> v & 1) && (nodes + a[v]) * (edges + e[u][v]) < (LL)f[v][s | (1 << v)] * g[v][s | (1 << v)]) {
