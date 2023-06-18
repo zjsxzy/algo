2f052445ab2d9af94bc51d7c0b99c2546dbe9175
zhangyang <zjsxzy@gmail.com>
2023-06-18 20:52:55 +0800

    add arc 162

--- AtCoder/AtCoder Regular Contest 162/A.cpp
+++ AtCoder/AtCoder Regular Contest 162/A.cpp
@@ -3,13 +3,31 @@ using namespace std;
 typedef long long LL;
 
 void solve() {
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        bool flag = true;
+        for (int j = i + 1; j < n; j++) {
+            if (p[j] < p[i]) {
+                flag = false;
+                break;
+            }
+        }
+        res += flag;
+    }
+    cout << res << endl;
 }
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     int ts = 1;
-    // cin >> ts;
+    cin >> ts;
     for (int t = 1; t <= ts; t++) {
         solve();
     }
