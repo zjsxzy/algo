f92d8940ee61e4e9006e7b3cd07a2a117376eeb7
Zhang Yang <zjsxzy@gmail.com>
2023-09-25 09:41:45 +0800

    upd c

--- Facebook/HackerCup-2023/Practice Round/C.cpp
+++ Facebook/HackerCup-2023/Practice Round/C.cpp
@@ -7,12 +7,36 @@ void solve(int cs) {
     int n;
     cin >> n;
     vector<int> a(2 * n - 1);
-    LL sum = 0;
+    multiset<int> st;
     for (auto& x: a) {
         cin >> x;
-        sum += x;
+        st.insert(x);
     }
     sort(a.begin(), a.end());
+    if (n == 1) {
+        cout << a[0] << endl;
+        return;
+    }
+    const int inf = 2e9 + 1;
+    auto f = [&](int k) {
+        multiset<int> s = st;
+        int res = -1;
+        while (!s.empty()) {
+            auto x = s.begin();
+            int val = *x;
+            s.erase(x);
+            if (s.count(k - val)) s.extract(k - val);
+            else if (res != -1) return inf;
+            else res = k - val; 
+        }
+        if (res > 0) return res;
+        return inf;
+    };
+    int res = f(a[0] + a.back());
+    res = min(res, f(a[1] + a.back()));
+    res = min(res, f(a[0] + a[2 * n - 3]));
+    if (res == inf) res = -1;
+    cout << res << endl;
 }
 
 int main() {
