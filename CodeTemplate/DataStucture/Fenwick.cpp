b614e2540e533b9774d1425a517a72581f701f48
Zhang Yang <zjsxzy@gmail.com>
2024-06-10 14:24:16 +0800

    Update Fenwick.cpp

--- CodeTemplate/DataStucture/Fenwick.cpp
+++ CodeTemplate/DataStucture/Fenwick.cpp
@@ -25,3 +25,43 @@ struct Fenwick{
         return get(r) - get(l - 1);
     }
 };
+
+
+// 区间修改区间查询
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n + 1);
+    Fenwick d(n + 2), dk(n + 2); // d[i] = a[i] - a[i-1], dk[i] = d[i] * i
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        d.upd(i, a[i] - a[i - 1]);
+        dk.upd(i, (LL)i * (a[i] - a[i - 1]));
+    }
+
+    auto range_upd = [&](int l, int r, LL x) {
+        d.upd(l, x);
+        d.upd(r + 1, -x);
+        dk.upd(l, x * l);
+        dk.upd(r + 1, -x * (r + 1));
+    };
+    auto get_sum = [&](int r) {
+        return d.get(r) * (r + 1) - dk.get(r);
+    };
+    auto range_sum = [&](int l, int r) {
+        return get_sum(r) - get_sum(l - 1);
+    };
+
+    while (q--) {
+        int t, l, r, x;
+        cin >> t;
+        if (t == 1) {
+            cin >> l >> r >> x;
+            range_upd(l, r, x);
+        } else {
+            cin >> l >> r;
+            cout << range_sum(l, r) << endl;
+        }
+    }
+}
