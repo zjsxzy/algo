2e6dfffb565421e60f53cfbbbdd3dcceee2ca678
Zhang Yang <zjsxzy@gmail.com>
2024-07-05 19:51:44 +0800

    upd quickpow

--- CodeTemplate/Math/quickpow.cpp
+++ CodeTemplate/Math/quickpow.cpp
@@ -1,4 +1,4 @@
-LL quickpow(LL x, LL n, LL mod) {
+LL qpow(LL x, LL n) {
     LL res = 1;
     for (; n; n >>= 1) {
         if (n & 1) res = res * x % mod;
@@ -7,3 +7,9 @@ LL quickpow(LL x, LL n, LL mod) {
     return res;
 }
 
+// 等比数列求和
+LL sum(LL a, LL x) {
+    if (x == 1) return a % mod;
+    else if(x & 1) return ( sum(a, x / 2) % mod + ( (sum(a, x / 2) % mod) * (qpow(a, x / 2) % mod) ) + qpow(a, x) % mod ) % mod;
+    else return ( sum(a, x / 2) % mod + ( (sum(a, x / 2) % mod) * (qpow(a, x / 2) % mod) ) ) % mod;
+}
